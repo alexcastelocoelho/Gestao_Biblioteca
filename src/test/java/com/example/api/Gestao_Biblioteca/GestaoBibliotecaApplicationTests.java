cfba2c4df9146dcb24d1b9b23adeb7a523f5f8f9
Alex Coelho <alexcastelo437@gmail.com>
2024-09-26 18:25:23 -0300

    feat: Definindo estrutura inicial do projeto

---
+++ src/test/java/com/example/api/Gestao_Biblioteca/GestaoBibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.api.Gestao_Biblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoBibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
