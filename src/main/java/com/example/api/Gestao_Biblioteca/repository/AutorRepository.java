863b7439dd529c469a705049700fea8887689065
Alex Coelho <alexcastelo437@gmail.com>
2024-09-26 19:22:44 -0300

    feat: Criação de autor repository

---
+++ src/main/java/com/example/api/Gestao_Biblioteca/repository/AutorRepository.java
@@ -0,0 +1,11 @@
+package com.example.api.Gestao_Biblioteca.repository;
+
+import com.example.api.Gestao_Biblioteca.model.AutorModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface AutorRepository extends JpaRepository<AutorModel, UUID> {
+}
