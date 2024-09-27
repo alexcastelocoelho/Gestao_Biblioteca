b16c30d5b6fa87bf9cf5c5da4d80c797719ade38
Alex Coelho <alexcastelo437@gmail.com>
2024-09-26 23:47:56 -0300

    feat: Adicionando checagem e exceção para erro na validação do dto

--- src/main/java/com/example/api/Gestao_Biblioteca/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/api/Gestao_Biblioteca/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.example.api.Gestao_Biblioteca.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
@@ -20,6 +21,11 @@ public class GlobalExceptionHandler {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("O id fornecido é muito grande, verifique o formato inserido");
         }
          return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Erro de argumento inválido.");
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> methodArgumentNotValidException(MethodArgumentNotValidException exception) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getBindingResult().getFieldError().getDefaultMessage());
     }
 
 }
