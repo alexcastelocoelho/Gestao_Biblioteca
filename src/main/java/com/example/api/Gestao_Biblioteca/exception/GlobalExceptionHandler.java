e0137f7142433ed52961c54229dd7683b6ee3c36
Alex Coelho <alexcastelo437@gmail.com>
2024-09-26 23:03:48 -0300

    feat: Verificação de id caso argumento seja invalido

--- src/main/java/com/example/api/Gestao_Biblioteca/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/api/Gestao_Biblioteca/exception/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -11,6 +12,14 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(AutorNaoLocalizado.class)
     public ResponseEntity<String> autorNaoLocalizado(AutorNaoLocalizado exception) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<String> methodArgumentTypeMismatchException(MethodArgumentTypeMismatchException exception) {
+        if (exception.getMessage() != null && exception.getMessage().contains("UUID string too large")) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("O id fornecido é muito grande, verifique o formato inserido");
+        }
+         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Erro de argumento inválido.");
     }
 
 }
