023a975f364b74645199c81e04401cc28e2de102
Alex Coelho <alexcastelo437@gmail.com>
2024-10-01 22:16:20 -0300

    feat: Criado enum para campo genero

---
+++ src/main/java/com/example/api/Gestao_Biblioteca/utils/enums/Genero.java
@@ -0,0 +1,14 @@
+package com.example.api.Gestao_Biblioteca.utils.enums;
+
+public enum Genero {
+    FICCAO,
+    FANTASIA,
+    MISTERIO,
+    ROMANCE,
+    TERROR,
+    HISTORIA,
+    CIENCIA,
+    AUTOAJUDA,
+    BIOGRAFIA
+
+}
